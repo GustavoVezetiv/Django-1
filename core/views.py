abed4b00dddc7dab8ba3825022e412902b0f6444
Gustavo Vezetiv <142027309+GustavoVezetiv@users.noreply.github.com>
2025-03-11 17:18:24 -0400

    criar pg produto - tabela

--- core/views.py
+++ core/views.py
@@ -2,11 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    produtos = Produtos.objects.all()
+
     context = {
         'curso' : 'Programar web com django',
         'outro' : 'Django é massa',
+        'produto' : produtos
     }
     return render(request, 'index.html', context)
 
 def contato(request):
     return render(request, 'contato.html')
+
+def produto(request):
+    return render(request, 'produto.html')
